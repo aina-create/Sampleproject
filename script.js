08e041c755033572b88af55d23bac7571d2f5ef1
aina-create <ainavt23@gmail.com>
2026-06-15 12:07:42 +0530

    prediction

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let button = document.querySelector("button");
+let predict = document.querySelector("prediction");
+
+
+
 let moved = 0;
 button.addEventListener("mouseover", function () {
   if (!moved) {
@@ -8,4 +12,9 @@ button.addEventListener("mouseover", function () {
     button.style.marginLeft = "0px";
     moved = false;
   }
-});
+})
+
+setTimeout(function(){
+    button.style.direction = "none"
+    predict.innerText = "sorry... you dont have a future"
+},10000)
